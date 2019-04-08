1f6edfa4daacdd937a35fed3a1bd98c4ac523f46
Alpen Adria Universität Klagenfurt <corinnagr@edu.aau.at>
2019-04-08 14:02:21 +0200

    show list from current games
    Created classes Player and Game which are neccessery for storing data. Showing current games in Class GameList. Implemented it with a ListAdapter and a viewHolder

---
+++ app/src/main/java/com/example/scotlandyard/Game.java
@@ -0,0 +1,51 @@
+package com.example.scotlandyard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    private String gameName;
+    private int maxMembers;
+    private int currentMembers;
+    private List<Player> players = new ArrayList<>();
+
+
+    public Game(String gameName, int maxMembers) {
+        this.gameName = gameName;
+        this.maxMembers = maxMembers;
+        this.currentMembers = 1;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public int getMaxMembers() {
+        return maxMembers;
+    }
+
+    public void setMaxMembers(int maxMembers) {
+        this.maxMembers = maxMembers;
+    }
+
+    public int getCurrentMembers() {
+        return currentMembers;
+    }
+
+    public void setCurrentMembers(int currentMembers) {
+        this.currentMembers = currentMembers;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+}
--- app/src/main/java/com/example/scotlandyard/MainActivity.java
+++ app/src/main/java/com/example/scotlandyard/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.scotlandyard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button createGame;
+    Button existingGames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        existingGames = findViewById(R.id.btnExistingGames);
+        existingGames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameList.class));
+            }
+        });
     }
 }
