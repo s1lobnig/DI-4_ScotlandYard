2b47c76c6b5903b9e3c4c4892c2fa12614822160
Alpen Adria Universität Klagenfurt <corinnagr@edu.aau.at>
2019-04-30 09:27:47 +0200

    imported classes

--- app/src/main/java/com/example/scotlandyard/Messanger.java
+++ app/src/main/java/com/example/scotlandyard/Messanger.java
@@ -1,5 +1,6 @@
 package com.example.scotlandyard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,8 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.example.scotlandyard.connection.ClientInterface;
+import com.example.scotlandyard.connection.ClientService;
 import com.example.scotlandyard.connection.Endpoint;
 import com.example.scotlandyard.connection.ServerInterface;
+import com.example.scotlandyard.connection.ServerService;
+
 import java.util.Map;
 
 public class Messanger extends AppCompatActivity implements ServerInterface, ClientInterface{
