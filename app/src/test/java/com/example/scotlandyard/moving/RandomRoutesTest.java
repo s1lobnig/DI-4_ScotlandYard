7b0fdb5d9de906d93b69ddb2223ac8814df518fb
r1rasser <43993461+r1rasser@users.noreply.github.com>
2019-06-09 22:43:30 +0200

    testcases for random routes

---
+++ app/src/test/java/com/example/scotlandyard/moving/RandomRoutesTest.java
@@ -0,0 +1,63 @@
+package com.example.scotlandyard.moving;
+
+import com.example.scotlandyard.map.Route;
+import com.example.scotlandyard.map.Routes;
+import com.example.scotlandyard.map.ValidatedRoute;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.scotlandyard.map.routefactories.BicycleRoutes.BICYCLE;
+import static com.example.scotlandyard.map.routefactories.BusRoutes.BUS;
+import static com.example.scotlandyard.map.routefactories.FootRoutes.BY_FOOT;
+import static com.example.scotlandyard.map.routefactories.TaxiRoutes.TAXI_DRAGAN;
+
+@RunWith(Parameterized.class)
+public class RandomRoutesTest {
+    @Parameterized.Parameters(name = "{index}: Bot at {0}, other players at {1}")
+    public static Iterable data() {
+        return Arrays.asList(new Object[][]{
+                /*
+                bot position
+                player positions
+                possible resulting routes
+                */
+                {1, 4, Arrays.asList(new Route[]{BY_FOOT[0]})},
+                {24, 27, Arrays.asList(new Route[]{BY_FOOT[17], BY_FOOT[20], BY_FOOT[25], BY_FOOT[27]})},
+                {1, 2, Arrays.asList(new Route[]{BICYCLE[0]})},
+        });
+    }
+
+    private int currentPosition;
+    private int notToInclude;
+    private List<Route> possibleResults;
+    private ValidatedRoute result;
+
+    public RandomRoutesTest(int currentPosition, int notToInclude, List<Route> possibleResults) {
+        this.currentPosition = currentPosition;
+        this.notToInclude = notToInclude;
+        this.possibleResults = possibleResults;
+    }
+
+    @Before
+    public void setUp() {
+        result = Routes.getRandomRoute(currentPosition, notToInclude);
+    }
+
+    @Test
+    public void testIfInPossibleRouteSet() {
+        Assert.assertTrue(possibleResults.contains(result.getRoute()));
+    }
+
+    @After
+    public void tearDown() {
+        result = null;
+    }
+}
