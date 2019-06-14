87b28c25268e99037246b0b702c285dc7444fd32
r1rasser <43993461+r1rasser@users.noreply.github.com>
2019-06-14 19:08:48 +0200

    Background Music Service

---
+++ app/src/main/java/com/example/scotlandyard/MusicService.java
@@ -0,0 +1,115 @@
+package com.example.scotlandyard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnErrorListener;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MusicService extends Service implements MediaPlayer.OnErrorListener {
+
+    private static final String TAG = "MusicService";
+
+    private static MediaPlayer mPlayer;
+    private static int length = 0;
+
+    public MusicService() {
+    }
+
+    private final IBinder binder = new LocalBinder();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        public MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mPlayer = MediaPlayer.create(this, R.raw.scotland_yard_bg_music);
+        mPlayer.setOnErrorListener(this);
+
+        if (mPlayer != null) {
+            mPlayer.setLooping(true);
+            mPlayer.setVolume(100, 100);
+        }
+
+
+        mPlayer.setOnErrorListener(new OnErrorListener() {
+
+            public boolean onError(MediaPlayer mp, int what, int
+                    extra) {
+
+                onError(mPlayer, what, extra);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mPlayer.start();
+        return START_STICKY;
+    }
+
+    public void pauseMusic() {
+        if (mPlayer.isPlaying()) {
+            mPlayer.pause();
+            length = mPlayer.getCurrentPosition();
+            Log.d(TAG, "Pause at " + length);
+        }
+    }
+
+    public void resumeMusic() {
+        if (mPlayer != null && mPlayer.isPlaying() == false) {
+            Log.d(TAG, "Resume from " + length);
+            mPlayer.seekTo(length);
+            mPlayer.start();
+        }
+    }
+
+    public void stopMusic() {
+        mPlayer.stop();
+        mPlayer.release();
+        mPlayer = null;
+        Log.d(TAG, "Stop music");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mPlayer != null) {
+            try {
+                mPlayer.stop();
+                mPlayer.release();
+            } finally {
+                mPlayer = null;
+            }
+        }
+    }
+
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        if (mPlayer != null) {
+            try {
+                mPlayer.stop();
+                mPlayer.release();
+            } finally {
+                mPlayer = null;
+            }
+        }
+        return false;
+    }
+}
